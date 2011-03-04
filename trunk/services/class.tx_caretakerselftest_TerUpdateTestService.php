bf4766ccefc7c2ec04c3d8c892bd2ba4f117343e
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-04 13:38:55 +0000

    * adjustes selftest ter update test for 4.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@44628 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/services/class.tx_caretakerselftest_TerUpdateTestService.php
+++ trunk/services/class.tx_caretakerselftest_TerUpdateTestService.php
@@ -50,17 +50,52 @@ class tx_caretakerselftest_TerUpdateTestService extends tx_caretaker_TestService
 
 	public function runTest() {
 
-		$config = $this->getConfiguration();
+		if ( t3lib_div::int_from_ver( TYPO3_version ) < t3lib_div::int_from_ver( '4.5.0' ) ){
+			return $this->runTest400();
+		} else {
+			return $this->runTest450();
+		}
 		
+	}
+
+	private function runTest400(){
+		
+		$config = $this->getConfiguration();
+
 		$mTime = filemtime(PATH_site . 'typo3temp/extensions.xml.gz');
 		$age   = time() - $mTime;
 		
 		if ( $age > $config['maxAge'] ){
-			return tx_caretaker_TestResult::create( tx_caretaker_Constants::state_error, $age, 'TER-Update is due. Last update was  on '.strftime ( '%x %X' , $mTime)  );
+			return tx_caretaker_TestResult::create( tx_caretaker_Constants::state_error, $age, 'TER-Update is due. Last update was on '.strftime ( '%x %X' , $mTime)  );
 		} else {
-			return tx_caretaker_TestResult::create( tx_caretaker_Constants::state_ok, $age, 'TER is up to Date. Last update was  on '.strftime ( '%x %X' , $mTime) );
+			return tx_caretaker_TestResult::create( tx_caretaker_Constants::state_ok, $age, 'TER is up to Date. Last update was on '.strftime ( '%x %X' , $mTime) );
 		}
 		
+	}
+
+	private function runTest450(){
+		$config = $this->getConfiguration();
+		$minTimstamp = time() - $config['maxAge'];
+		
+		$repositories = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows ( '*', 'sys_ter', '1' );
+		
+		$errors = array();
+		$ok = array();
+
+		foreach( $repositories as $repository ){
+			if ( $repository['lastUpdated'] < $minTimstamp ){
+				$errors[] = new tx_caretaker_ResultMessage( 'ERROR: Extension-repository ' . $repository['title'] . ' was not up to date. Last update was on ' . strftime ( '%x %X' ,  $repository['lastUpdated'] ) );
+			} else {
+				$ok = new tx_caretaker_ResultMessage( 'OK: Extension-repository ' . $repository['title'] . ' is up to date. Last update was on ' . strftime ( '%x %X' ,  $repository['lastUpdated'] ) );
+			}
+		}
+		
+		if ( count($errors) > 0 ){
+			return tx_caretaker_TestResult::create( tx_caretaker_Constants::state_error, $age, count($errors) . ' extension repositories have to be updated. ' . count($ok)  . ' extension repositories are ok. ' );
+		} else {
+			return tx_caretaker_TestResult::create( tx_caretaker_Constants::state_ok, $age, count($ok)  . ' extension repositories are ok. ' );
+		}
+
 	}
 
 	private function getConfiguration(){
